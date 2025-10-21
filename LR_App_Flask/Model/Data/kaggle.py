ead36bfb8ac52f6999b5df06a471600153d80487
soumya-569 <soumya.p569official@gmail.com>
2025-10-21 22:24:18 +0530

    Folder added and renamed

--- LR_App_Flask/Model/Data/kaggle.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ["KAGGLEHUB_CACHE"] = r"F:\Udemy\Git\ML Portfolio\LR_App_Flask\Model\Data"
-import kagglehub
-
-path = kagglehub.dataset_download("shalmamuji/electricity-cost-prediction-dataset")
-
-print(path)
