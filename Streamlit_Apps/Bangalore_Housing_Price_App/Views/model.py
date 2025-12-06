ced9f04f864b18b71ced0da7ebf5375190ff8e1d
soumya-569 <soumya.p569official@gmail.com>
2025-12-06 20:58:09 +0530

    Changes In App

--- Streamlit_Apps/Bangalore_Housing_Price_App/Views/model.py
+++ Streamlit_Apps/Bangalore_Housing_Price_App/Views/model.py
@@ -47,6 +47,6 @@ with st.form(key="ml_form"):
             new_data_point = pd.DataFrame([[Area_Type,loc_value,Segment,bhk,sqft,bath_count,balcony_count]],columns=feature_cols)
             price_predict = model.predict(new_data_point)[0]
             st.success(f"Based On Your Choice, House Price Will Be : ₹{price_predict*100000:,.0f}")
-            st.plotly_chart(price_predict,Location,location_wise_average(Location))
+            st.plotly_chart(price_comparison_chart(price_predict,Location,location_wise_average(Location)))
     else:
         st.info("Fill Out The Form To Get Your House Price")
